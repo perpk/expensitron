1a4b559c8151990ce18397de487e399d910b1816
kperp <k.a.perperidis@gmail.com>
2018-01-15 00:36:30 +0200

    completed tests for master-record

--- specs/master_data_spec.clj
+++ specs/master_data_spec.clj
@@ -4,14 +4,25 @@
             [backend.repos.master.master-type :refer :all]
             [backend.repos.master.master-data :refer :all]))
 
+(def record-name "wal-mart")
+
 (describe "testing masterdata record CRUDs"
           (before-all
             (println "start testing...")
             (def type (create-masterdata-type-route {:type "supermarket"})))
           (it "record should not be nil"
-              (def record (create-masterdata-record {:name "wal-mart" :type_id ((first type) :id)}))
-              (should-not (nil? record))
-              )
+              (def record (create-masterdata-record {:name record-name :type_id ((first type) :id)}))
+              (should-not (nil? record)))
+          (it "record should have an id"
+              (should-not (nil? ((first record) :id))))
+          (it "record should reference a type"
+              (should-not (nil? ((first record) :id))))
+          (it "record should reference the type it was created for"
+              (should (= ((first type) :id) ((first record) :type_id))))
+          (it "record should have a name"
+              (should-not (nil? ((first record) :name))))
+          (it "record should have the same name as the one it was created with"
+              (should (= record-name ((first record) :name))))
           (after-all
             (delete-masterdata-by-id ((first record) :id))
             (delete-masterdata-type-by-id ((first type) :id))
